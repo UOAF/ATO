fa6b5dc2b1ae7007044f89c4b146235921ba1a37
Bobby Moretti <bobmoretti@gmail.com>
2020-09-24 20:06:59 -0700

    Added flask server with discord SSO example.

---
+++ backend/server.py
@@ -0,0 +1,52 @@
+import os
+
+from flask import Flask, redirect, url_for
+from flask_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+app.secret_key = b"random bytes representing flask secret key"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"      # !! Only in development environment.
+
+# app.config["DISCORD_CLIENT_ID"] =     # Discord client ID.
+# app.config["DISCORD_CLIENT_SECRET"] = ""                # Discord client secret.
+# app.config["DISCORD_REDIRECT_URI"] = ""                 # URL to your callback endpoint.
+# app.config["DISCORD_BOT_TOKEN"] = ""                    # Required to access BOT resources.
+
+discord = DiscordOAuth2Session(app)
+
+
+@app.route("/login/")
+def login():
+    return discord.create_session()
+	
+
+@app.route("/callback/")
+def callback():
+    discord.callback()
+    return redirect(url_for(".me"))
+
+
+@app.errorhandler(Unauthorized)
+def redirect_unauthorized(e):
+    return redirect(url_for("login"))
+
+	
+@app.route("/me/")
+#@requires_authorization
+def me():
+    user = discord.fetch_user()
+    print("ASDASDASDD")
+    return f"""
+    <html>
+        <head>
+            <title>{user.name}</title>
+        </head>
+        <body>
+            <img src='{user.avatar_url}' />
+        </body>
+    </html>"""
+
+
+if __name__ == "__main__":
+    app.run()
