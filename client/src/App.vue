c9345a0aca2daccdd7f432b9cc750e350d89de6a
Bobby Moretti <bobmoretti@gmail.com>
2020-09-24 18:04:51 -0700

    Initial commit of client

---
+++ client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+    <employee-table />
+  </div>
+</template>
+
+<script>
+import EmployeeTable from '@/components/EmployeeTable.vue'
+
+export default {
+  name: 'app',
+  components: {
+    EmployeeTable
+  }
+}
+</script>
+
+<style>
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
+</style>
